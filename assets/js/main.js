6c4415673525550f7dbdca81a13c9fe442642669
buitantoan <50437208+buitantoan@users.noreply.github.com>
2022-01-11 22:35:05 +0700

    create header and slide ver desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,29 @@
 ( function( $ ) {
 
     $(document).ready(function () {
+
+        var swiper = $('.swiper');
+        var swiper_data = {
+            spaceBetween: 30,
+            slidesPerView: 1,
+            centeredSlides: true,
+            // autoplay: {
+            //     delay: 3000,
+            //     disableOnInteraction: false,
+            // },
+            pagination: {
+              el: ".swiper-pagination",
+              clickable: true,
+            },
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+        };
+
+        var swiperHandle = new Swiper(swiper, swiper_data);
+
+
         var navMenu = $('.wd-menu');
         navMenu.on('click', '.menu-item a', function (e) {
            console.log($(this)); 
